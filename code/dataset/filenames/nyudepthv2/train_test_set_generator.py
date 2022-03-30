ff9ed428e5c1355ec8c19f0d1a837ace450aaeb7
Jeongwoo Seo <seoj4@cn1511.vampire>
2022-03-29 21:21:10 -0500

    fixes to generator

--- code/dataset/filenames/nyudepthv2/train_test_set_generator.py
+++ code/dataset/filenames/nyudepthv2/train_test_set_generator.py
@@ -1,21 +1,25 @@
 #creates a txt file of training images, a subset of all available training images
 #selected images are evenly distributed from total samples
+import os
+
 
 #enter skip number for subset sizes (results will be approximate)
 train_subset_size = 1000
 #enter percentage of training set size to be the test set (eg 10 = 10%, max 100% obviously)
 test_subset_percentage = 20
+script_dir = os.path.join(os.path.dirname(__file__),str(train_subset_size))
+os.makedirs(script_dir, exist_ok=True)
 
 train_skip = train_subset_size/100
 test_subset_size = int(train_subset_size*(test_subset_percentage*0.01))
 test_skip = test_subset_size/100
 
-train_file_name = 'train_subset_x' + str(train_subset_size) + '.txt'
-test_file_name = 'test_subset_x' + str(test_subset_size) + '.txt'
+train_file_name = 'train_subset.txt'
+test_file_name = 'test_subset.txt'
 
-train = open(train_file_name, 'w')
-test = open(test_file_name, 'w')
-with open('train_list.txt', 'r') as r:
+train = open(os.path.join(script_dir,train_file_name), 'w')
+test = open(os.path.join(script_dir,test_file_name), 'w')
+with open(os.path.join(os.path.dirname(__file__),'train_list.txt'), 'r') as r:
     sorted_r = sorted(r)
     desired_train = sorted_r[0:len(sorted_r)-1:round(242.3/train_skip)]
     desired_test = sorted_r[1::round(242.3/test_skip)]
